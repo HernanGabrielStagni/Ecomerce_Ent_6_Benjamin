5e2f229feb9f578f5e53a3821156e1209a27f2ab
HernanGabrielStagni <psicologohernanstagni@gmail.com>
2023-11-12 17:24:51 -0300

    Le hice el register con el post al backend

--- src/COMPONENTS/SHARED/Header.jsx
+++ src/COMPONENTS/SHARED/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "./Header_CSS.css"
 
 const Header = () => {
   return (
-    <header>
-        <h1><Link to='/' >Ecomerce</Link></h1>
+    <header className='container__header'>
+        <h1><Link className='Link' to='/' >Ecomerce</Link></h1>
         <nav>
-            <ul>
+            <ul className='menu'>
                 <li><Link to='/' >Login</Link></li>
                 <li><Link to='/register'>Register</Link></li>
                 <li><Link to='/'>Purchase</Link></li>
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import useAuthentication from '../HOOKS/useAuthentication'
 
 const Register = () => {
+
+
+const {register, handleSubmit, reset} = useForm()
+
+const {createNewUser}=useAuthentication()
+ 
+const submit = (data) =>{
+  // console.log(data)
+  createNewUser(data)
+
+} 
+
   return (
-    <div>Register</div>
-  )
+    <form onSubmit={handleSubmit(submit)}>
+    <h1>Create a new</h1>
+      <div>
+        <label htmlFor="firstName">First Name</label>
+        <input {...register('firstName')} type="text"  id='firstName' />
+      </div>
+    
+      <div>
+        <label htmlFor="lastName">Last  Name</label>
+        <input {...register('lastName')} type="text"  id='lastName' />
+      </div>
+
+      <div>
+        <label htmlFor="email">Email</label>
+        <input {...register('email')} type="email"  id='email' />
+      </div>
+
+      <div>
+        <label htmlFor="password">Password</label>
+        <input {...register('password')} type="password"  id='password' />
+      </div>
+
+      <div>
+        <label htmlFor="phone">Phone</label>
+        <input {...register('phone')} type="tel"  id='phone' />
+      </div>
+    
+|     <button>Register</button>    
+    </form>
+    )
 }
 
 export default Register
